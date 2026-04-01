dbe6ccfc0066ef722565df440d07b39b1176fc27
Mark Jensen <goldenjigsaw126@gmail.com>
2026-04-01 13:49:46 -0600

    Progress up to this point. I realize that I forgot to commit the past few times I have worked on this. So far I have been able to get everything to work the way it is supposed to meaning every page is in some way shape or form made or close to being done. I am in the process of finish the user profile and backend work

---
+++ Gold Golf Turnoments/Gold Golf Turnoments/Network.swift
@@ -0,0 +1,18 @@
+//
+//  Network.swift
+//  Gold Golf Turnoments
+//
+//  Created by Mark Jensen on 4/1/26.
+//
+
+import SwiftUI
+
+struct Network: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Network()
+}
---
+++ Gold Golf Turnoments/Gold Golf Turnoments/UserProfile.swift
@@ -0,0 +1,18 @@
+//
+//  UserProfile.swift
+//  Gold Golf Turnoments
+//
+//  Created by Mark Jensen on 4/1/26.
+//
+
+import SwiftUI
+
+struct UserProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserProfile()
+}
