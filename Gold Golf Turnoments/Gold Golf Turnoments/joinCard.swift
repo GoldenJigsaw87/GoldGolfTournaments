34958813d747cb365a5fd966c55dea384ee00497
Mark Jensen <goldenjigsaw126@gmail.com>
2026-04-16 13:30:36 -0600

    Official backend progress as well as some frontend changes such as background, scorecard sizing fixes, and pages. Major issue being the backend not storing passwords

---
+++ Gold Golf Turnoments/Gold Golf Turnoments/joinCard.swift
@@ -0,0 +1,18 @@
+//
+//  joinCard.swift
+//  Gold Golf Turnoments
+//
+//  Created by Mark Jensen on 4/9/26.
+//
+
+import SwiftUI
+
+struct joinCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    joinCard()
+}
